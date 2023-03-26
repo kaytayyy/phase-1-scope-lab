a003bc2f9b93fd80919d4a360f2acf25b2a15ec4
Inda Smith <inda.k.smith@gmail.com>
2023-03-26 16:49:53 -0400

    first test is passing, but not the rest. not sure if it's a problem with my code or my computer.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function uppercaseCustomerName(customerName) {
+    let uppercase = customerName.toUpperCase;
+    return uppercase
+}
+
+function setBestCustomer(customerName) {
+return "not" + customerName;
+}
